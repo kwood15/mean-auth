9489356418decac8b2b725ab7ed6beeea62bec49
Kay Wood <11405108+kwood15@users.noreply.github.com>
2018-10-15 13:11:16 +0100

    Add new components and auth service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'mean-auth';
+  constructor(public auth: AuthenticationService) {}
 }
